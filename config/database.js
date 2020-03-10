c9e9ae9794ed7be2a2d0cafd0f3db7a78d9bcf37
Aliaa-Hosny <aliaahosny77@gmail.com>
2020-03-10 04:24:35 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb+srv://root:5cf8z77mSCoMC97C@cluster0-igmlf.mongodb.net/test?retryWrites=true&w=majority' || 'mongodb://localhost:27017'
+	url : 'mongodb://root:5cf8z77mSCoMC97C@cluster0-igmlf.mongodb.net' || 'mongodb://localhost:27017'
 }
